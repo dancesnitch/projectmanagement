72a7cb448041beccded9eb88c59ab1824beaa958
Daniel Byrne <byrne.daniel87@gmail.com>
2017-07-29 19:37:50 -0400

    added projects page, login redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
 end
+
+#Make user login/signup before landing page
+#before_action :authenticate_user!
+
